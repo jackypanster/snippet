5ea00d51fd6e48ce30e81cc21b59c27200f73621
zhibinpan <jackypanster@gmail.com>
2017-11-08 22:41:28 +0800

    add restful server and client side

--- client/main.go
+++ client/main.go
@@ -3,14 +3,17 @@ package main
 import (
 	"github.com/parnurzeal/gorequest"
 	"log"
+	"net/http"
 )
 
 func main() {
 	request := gorequest.New()
 	resp, body, errs := request.Get("http://127.0.0.1:8080/").End()
 	if errs != nil {
-		log.Printf("%+v, %s, error: %+v", resp, body, errs)
+		log.Printf("error: %+v", errs)
+	} else if resp.StatusCode != http.StatusOK {
+		log.Printf("%s, %s", resp.Status, body)
 	} else {
-		log.Printf("%+v, %s, %+v", resp.StatusCode, body, errs)
+		log.Printf("%s, %s", resp.Status, body)
 	}
 }
