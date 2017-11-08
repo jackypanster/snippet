ef68c9742e128173cff128d474ad961e91a6d872
zhibinpan <jackypanster@gmail.com>
2017-11-08 22:26:08 +0800

    add restful server and client side

--- client/main.go
+++ client/main.go
@@ -7,10 +7,10 @@ import (
 
 func main() {
 	request := gorequest.New()
-	resp, body, errs := request.Get("http://127.0.0.1:8080/pingg").End()
+	resp, body, errs := request.Get("http://127.0.0.1:8080/").End()
 	if errs != nil {
 		log.Printf("%+v, %s, error: %+v", resp, body, errs)
 	} else {
-		log.Printf("%+v, %s, %+v", resp.Status, body, errs)
+		log.Printf("%+v, %s, %+v", resp.StatusCode, body, errs)
 	}
 }
