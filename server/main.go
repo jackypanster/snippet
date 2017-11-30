7f4b02e5ebbff1768fcd549d97e4963575657570
zhibinpan <jackypanster@gmail.com>
2017-11-30 23:44:27 +0800

    update server usage

--- server/main.go
+++ server/main.go
@@ -1,13 +1,28 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"os"
+	"io"
+	"log"
+)
 
 func main() {
 	r := gin.Default()
+
+	f, _ := os.Create("gin.log")
+	gin.DefaultWriter = io.MultiWriter(f)
+
+	r.Use(gin.Logger())
+	r.Use(gin.Recovery())
+
 	r.GET("/ping", func(c *gin.Context) {
+		log.Printf("error %s", "testing")
+
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
+
 	r.Run() // listen and serve on 0.0.0.0:8080
 }
